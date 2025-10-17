a7c12c7fc9456e37800069ad16e216712b84c084
Elena Ignatenko <elignatenko1@gmail.com>
2025-10-17 08:00:44 +0200

    add wandb

--- src/captions_game/train.py
+++ src/captions_game/train.py
@@ -7,6 +7,7 @@ import argparse
 import os
 
 import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
 
 import egg.core as core
 # from egg.zoo.signal_game.archs import InformedSender, Receiver
@@ -15,6 +16,7 @@ from emcomm_captions.src.captions_game.features import CaptionsLoader
 from emcomm_captions.src.captions_game.archs import InformedSender, Receiver
 from datasets import load_from_disk, Dataset
 from emcomm_captions.src.captions_game.reinforce_wrappers import RnnSenderReinforce, RnnReceiverReinforce, SenderReceiverRnnReinforce
+from src.captions_game.trainers import Trainer
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -39,6 +41,11 @@ def parse_arguments():
         type=int,
         default=100,
         help="Batches in a single training/validation epoch",
+    )
+    parser.add_argument(
+        "--wandb",
+        action="store_true",
+        help="Logging to wandb",
     )
     parser.add_argument("--inf_rec", type=int, default=0, help="Use informed receiver")
     parser.add_argument(
@@ -111,13 +118,13 @@ def get_game(opt):
     return game
 
 
+# def log_opts(opts)
+
 if __name__ == "__main__":
     opts = parse_arguments()
-    # print(opts.batch_size)
-
     # data_folder = os.path.join(opts.root, "train/")
     # dataset = load_from_disk(data_folder)
-    dataset = load_from_disk(opts.root).select(range(3))
+    dataset = load_from_disk(opts.root).select(range(5))
 
     train_loader = CaptionsLoader(
         dataset,
@@ -143,12 +150,13 @@ if __name__ == "__main__":
         callbacks = []
 
     callbacks.append(core.ConsoleLogger(as_json=True, print_train_loss=True))
-    trainer = core.Trainer(
+    trainer = Trainer(
         game=game,
         optimizer=optimizer,
         train_data=train_loader,
         validation_data=validation_loader,
         callbacks=callbacks,
+        opts=opts,
     )
 
     print(opts)
---
+++ src/captions_game/trainers.py
@@ -0,0 +1,133 @@
+import os
+import pathlib
+
+import wandb
+from typing import List, Optional
+
+
+import torch
+from torch.utils.data import DataLoader
+
+# from .batch import Batch
+from egg.core.callbacks import (
+    Callback,
+    Checkpoint,
+    CheckpointSaver,
+    ConsoleLogger,
+    TensorboardLogger,
+    WandbLogger as CoreWandbLogger,
+)
+
+
+import argparse
+import json
+import os
+import pathlib
+import re
+import sys
+from collections import OrderedDict
+from typing import Any, Dict, Iterable, List, NamedTuple, Optional, Union
+
+import torch
+import wandb
+from rich.columns import Columns
+from rich.console import RenderableType
+from rich.progress import (
+    BarColumn,
+    Progress,
+    ProgressColumn,
+    TextColumn,
+    TimeRemainingColumn,
+)
+from rich.table import Table
+from rich.text import Text
+
+from egg.core.interaction import Interaction
+from egg.core.util import get_summary_writer
+
+
+# from .distributed import get_preemptive_checkpoint_dir
+# from .interaction import Interaction
+from egg.core.util import get_opts, move_to
+
+# try:
+#     from torch.cuda.amp import GradScaler, autocast
+# except ImportError:
+#     pass
+from egg.core.trainers import Trainer as CoreTrainer
+
+class WandbLogger(CoreWandbLogger):
+    def __init__(
+        self,
+        opts: Union[argparse.ArgumentParser, Dict, str, None] = None,
+        project: Optional[str] = 'emcomm',
+        run_id: Optional[str] = None,
+        **kwargs,
+    ):
+        super().__init__(opts=opts, project=project, run_id=run_id, **kwargs)
+
+    def _log_metrics(self, phase: str, loss: float, logs: Interaction, epoch: int):
+        """Helper for logging losses and auxiliary metrics."""
+        metrics = {f"{phase}/loss": loss, "epoch": epoch}
+        for k, v in logs.aux.items():
+            metrics[f"{phase}/{k}"] = v.mean()
+        self.log_to_wandb(metrics, commit=True)
+
+    def on_validation_end(self, loss: float, logs: Interaction, epoch: int):
+        if self.trainer.distributed_context.is_leader:
+            self._log_metrics("test", loss, logs, epoch)
+
+    def on_epoch_end(self, loss: float, logs: Interaction, epoch: int):
+        if self.trainer.distributed_context.is_leader:
+            self._log_metrics("train", loss, logs, epoch)
+
+
+class Trainer(CoreTrainer):
+    """
+        added wandb and whatever else would be needed
+    """
+
+    def __init__(
+        self,
+        game: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        train_data: DataLoader,
+        opts,
+        optimizer_scheduler: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
+        validation_data: Optional[DataLoader] = None,
+        device: torch.device = None,
+        callbacks: Optional[List[Callback]] = None,
+        grad_norm: float = None,
+        aggregate_interaction_logs: bool = True,
+        
+    ):
+        """
+        :param game: A nn.Module that implements forward(); it is expected that forward returns a tuple of (loss, d),
+            where loss is differentiable loss to be minimized and d is a dictionary (potentially empty) with auxiliary
+            metrics that would be aggregated and reported
+        :param optimizer: An instance of torch.optim.Optimizer
+        :param optimizer_scheduler: An optimizer scheduler to adjust lr throughout training
+        :param train_data: A DataLoader for the training set
+        :param validation_data: A DataLoader for the validation set (can be None)
+        :param device: A torch.device on which to tensors should be stored
+        :param callbacks: A list of egg.core.Callback objects that can encapsulate monitoring or checkpointing
+        """
+        super().__init__(
+            game,
+            optimizer,
+            train_data,
+            optimizer_scheduler,
+            validation_data,
+            device,
+            callbacks,
+            grad_norm,
+            aggregate_interaction_logs,
+        )
+        common_opts = get_opts()
+
+        if self.distributed_context.is_leader and common_opts.wandb:
+            # assert (
+            #     common_opts.tensorboard_dir
+            # ), "tensorboard directory has to be specified"
+            wandb_logger = WandbLogger(opts)
+            self.callbacks.append(wandb_logger)
